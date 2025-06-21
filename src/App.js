5ab86ae1db028d5f0d2fcbba33725e6202f2e2c6
rakeshkumar03 <97943594+rakeshkumar03@users.noreply.github.com>
2025-06-22 01:17:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ function App() {
 
   return (
     <div className="app-container">
-      <h1>Full-Stack Notes</h1>
+      <h1>Notes app</h1>
 
       {error && <div className="error-banner">{error}</div>}
 
